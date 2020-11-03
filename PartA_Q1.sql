cd37c44cc2c4916fb56904dd344e50ceb86f5c66
KayCave <73556270+KayCave@users.noreply.github.com>
2020-11-02 22:07:38 -0800

    Update PartA_Q1.sql

--- PartA_Q1.sql
+++ PartA_Q1.sql
@@ -1,3 +1,4 @@
+-- Write the query to create the 4 tables below.
 -- Question 1: --
 /*Create a table called client with the following columns:
 
@@ -66,6 +67,18 @@ FOREIGN KEY (`pid`) REFERENCES `project` (`id`),
 FOREIGN KEY (`eid`) REFERENCES `employee` (`id`),
 PRIMARY KEY (`pid`, `eid`)
 )ENGINE=InnoDB;
+
+-- Leave the queries below untouched. These are to test your submission correctly.
+-- Test that the tables were created
+DESCRIBE client;
+DESCRIBE employee;
+DESCRIBE project;
+DESCRIBE works_on;
+
+-- Test that the correct foreign keys were created 
+SELECT TABLE_NAME,COLUMN_NAME,CONSTRAINT_NAME,REFERENCED_TABLE_NAME,REFERENCED_COLUMN_NAME 
+FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE 
+WHERE REFERENCED_TABLE_SCHEMA = 'grade';
 
 
 
